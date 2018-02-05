b184ab1d542a43705f98892efe3572d1bd8f5a6d
Rasplicka <selek@seznam.cz>
2018-02-05 09:44:24 +0100

    prace 5/2/2018/2

--- structures.h
+++ structures.h
@@ -307,16 +307,3 @@ typedef struct
     char    internalState;          //state ridi start/data/end signal
    
 }I2CControl;
-
-
-
-typedef struct {
-    char* tx_buffer;
-    char* rx_buffer;
-    int tx_count;
-    int len; 
-    int inProcess;    
-    int working;
-    void* finish;
-} spiControl;
-
