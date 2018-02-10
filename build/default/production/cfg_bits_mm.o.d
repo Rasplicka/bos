a372a41695a511dc43bcdc35ced4cea2cb9a0bdf
Rasplicka <selek@seznam.cz>
2018-02-10 09:06:46 +0100

    doma working 10/2/2018 - 2

--- build/default/production/cfg_bits_mm.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/cfg_bits_mm.o:  \
-cfg_bits_mm.c  \
-globals.h  \
-def.h  \
- structures.h 
--- build/default/production/cfg_bits_mz.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/cfg_bits_mz.o:  \
-cfg_bits_mz.c  \
-globals.h  \
-def.h  \
- structures.h 
